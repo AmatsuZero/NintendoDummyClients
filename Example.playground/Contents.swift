b4187404a0c0685f112db013784e566eb9aa9c7d
jiangzhenhua <jiangzhenhua@baidu.com>
2018-11-06 20:57:24 +0800

    ADD: update

--- Example.playground/Contents.swift
+++ Example.playground/Contents.swift
@@ -1,2 +1,16 @@
 import Cocoa
 
+let shandShakeTemplate = """
+GET %@ HTTP/1.1
+Host: %d
+Upgrade: websocket
+Connection: upgrade
+Sec-WebSocket-Key: NEX
+Sec-WebSocket-Version: 13
+Sec-WebSocket-Protocol: NEX
+
+"""
+
+let handShake = String(format: shandShakeTemplate, "baidu", 80)
+
+print(handShake)
